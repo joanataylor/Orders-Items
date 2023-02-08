02b7376d46cd82604420740b939a8649286a7c47
Joana Grave <joana_grave@hotmail.com>
2023-02-07 22:14:46 -0500

    initial commit

---
+++ Order.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+public class Order {
+    public String name;
+    public boolean ready;
+    public double total;
+    public ArrayList<Item> items = new ArrayList<Item>();
+}
